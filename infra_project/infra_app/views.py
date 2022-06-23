5d8eae6b0514bbf0afdabae57421ecfc8a29c687
Pavel Vavilin <shtartora@gmail.com>
2022-06-23 12:14:48 +0500

    Workflow added

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('<center><b>У меня получилось!</b></center>')
 
 
 def second_page(request):
-    return HttpResponse('А это вторая страница')
+    return HttpResponse('<strong>А это вторая страница</strong>')
