8773e7e67d7b911f1c2695d7a6d21f798cdd67e1
Pavel Vavilin <shtartora@gmail.com>
2022-06-23 12:58:08 +0500

    fixe lines length

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,8 +2,12 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<html><body><center><b>У меня получилось!</b></center></body></html>")
+    return HttpResponse(
+        "<html><body><center><b>У меня получилось!</b></center></body></html>"
+    )
 
 
 def second_page(request):
-    return HttpResponse("<html><body><strong>А это вторая страница</strong></body></html>")
+    return HttpResponse(
+        "<html><body><strong>А это вторая страница</strong></body></html>"
+    )
